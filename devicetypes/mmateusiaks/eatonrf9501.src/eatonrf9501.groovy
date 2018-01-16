aa78231827e919c0afcc6272a4cdcbe082c7c4f2
Malgorzata Mateusiak <m.mateusiak@samsung.com>
2018-01-16 12:50:42 +0100

    Code with OFF delay = 10 s

--- devicetypes/mmateusiaks/eatonrf9501.src/eatonrf9501.groovy
+++ devicetypes/mmateusiaks/eatonrf9501.src/eatonrf9501.groovy
@@ -13,42 +13,12 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
- 
- 
- //zw:L type:1003 mfr:001A prod:534C model:0000 ver:3.21 zwv:3.67 lib:03 cc:25,27,75,86,70,71,85,77,2B,2C,72,73,82,87
- 
-/* 
- Switch Binary  0x25 V1  
- Switch All  0x27 V1  
- Protection  0x75 V1 V2  
- Version  0x86 V1  
- Configuration  0x70 V1 V2  
- Notification  0x71 V3  
- Association  0x85 V1 V2  
- Node Naming  0x77 V1  
- Scene Activation  0x2B V1  
- Scene Actuator Conf  0x2C V1  
- Manufacturer Specific  0x72 V1 V2 
- Powerlevel  0x73 V1  
- Hail  0x82 V1 
- Indicator  0x87 V1  
- 
- */
- 
- /*
- - ON?OFF
- - Programmable delayed OFF mode up to 4 minutes(default is 10 seconds
- - Child lockout feature
- - Configurable “power ON state”
- - Panic mode
- - Table 4. fro device specification : Device Configuration Parameters
- */
+
  
 metadata {
 	definition (name: "EatonRF9501", namespace: "MMateusiakS", author: "Malgorzata Mateusiak") {
 		capability "Switch"
         
-       
         fingerprint inClusters: "0x25,0x70"
         fingerprint mfr:"001A", prod:"534C", model:"0000"
 	}
@@ -71,22 +41,21 @@ metadata {
     }
 }
 
-// parse events into attributes
 def parse(String description) {
+
 	log.debug "Parsing description: ${description}"
     
-    def result = null;
-  
-  	def comand = zwave.parse(description);
+    def result = null
+  	def command = zwave.parse(description)
     
-    if(comand){
-    	result = zwaveEvent(comand);
-        log.debug "Parsed ${comand} to $result ${result.inspect()}"
+    if(command){
+    	result = zwaveEvent(command)
+        log.debug "Parsed ${command} to $result ${result.inspect()}"
     }else{
-    	log.debug "NO parsed event: ${description}";
+    	log.debug "NO parsed event: ${description}"
     }
     
-    return result;
+    return result
 
 }
 
@@ -94,17 +63,18 @@ def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicSet cmd)
 {
 
 	log.debug "Value of command is : ${cmd.value}" 
-    def result;
+    def result = null
+    
     if (cmd.value == 0) {
         result = createEvent(name: "switch", value: "off")
         log.debug "I send info to turn OFF"
         log.debug "basic set comand ${result}"
     }
+    
     if (cmd.value == 255){
         result = createEvent(name: "switch", value: "on")
         log.debug "I send info to turn ON"
-        log.debug "basic set comand ${result.linkText}"
-
+        log.debug "basic set comand ${result}"
     }
     
     return result
@@ -112,47 +82,43 @@ def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicSet cmd)
 
 
 def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd) {
-        def result = null;
-        
-        result = createEvent(name:"switch", value: cmd.value ? "on" : "off")
-        
-       
-        log.debug " zawave event SwitchBinaryReport ${result}"
-        
-        return result;
+    def result = null
+
+    result = createEvent(name:"switch", value: cmd.value ? "on" : "off")
+    log.debug " zawave event SwitchBinaryReport ${result}"
+
+    return result;
 
 }
 
 
 def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd)
 {
-        def result = null
-        result =  createEvent(name:"switch", value: cmd.value ? "on" : "off")
-		
-        
-        log.debug " zawave event basic report ${result}"
-        
-        return result;
+    def result = null
+    result =  createEvent(name:"switch", value: cmd.value ? "on" : "off")
+
+    log.debug " zawave event basic report ${result}"
+
+    return result
 
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.configurationv1.ConfigurationReport cmd){
 
     if(cmd){
-            log.debug "V1: ${cmd}"
-        }
+        log.debug "V1: ${cmd}"
+    }
 }
 
 
 def zwaveEvent(physicalgraph.zwave.commands.configurationv2.ConfigurationReport cmd){
 
     if(cmd){
-            log.debug "V2: ${cmd.configurationValue}"
-        	return createEvent( name : "configurationValue", value: cmd.configurationValue)
-        
-        }
+        log.debug "V2: ${cmd.configurationValue}"
+        return createEvent( name : "configurationValue", value: cmd.configurationValue)
+    }
 }
-// handle commands
+
 def on() {
 	log.debug "Executing 'on'"
    
@@ -180,12 +146,11 @@ def updated(){
     
     def cmds = configure1()
 
-    def hubComand = null;
-    hubComand = sendHubCommand(cmds)
+    if(cmds) sendHubCommand(cmds)
     
-    log.debug "***From updated and sendhub command: ${cmds}"
-
-    log.debug "***From updated and sendhub command: ${hubComand}"
+    log.debug "From updated and sendhub command: ${cmds}"
+    log.debug "From updated and sendhub command: ${hubComand}"
+    
 }
 
 
@@ -210,9 +175,9 @@ def initialize(){
 
 def configure1(){
 
-	def str = zwave.configurationV1.configurationSet(configurationValue: [0x7B], parameterNumber: 1, size: 1).format()
+	def str = zwave.configurationV1.configurationSet(configurationValue: [0x10], parameterNumber: 1, size: 1).format()
     
-    log.debug "${str}"
+    log.debug "Configuration info: ${str}"
     
 	return new physicalgraph.device.HubAction(str)
 
